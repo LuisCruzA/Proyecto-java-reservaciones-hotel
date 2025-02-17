311b85c43c6cf19fc2a552d12802d37b8705eee0
CDCZ25 <146612810+CDCZ25@users.noreply.github.com>
2025-02-17 09:58:18 -0600

    Update Habitacion.java

--- src/modelo/Habitacion.java
+++ src/modelo/Habitacion.java
@@ -9,5 +9,37 @@ package modelo;
  * @author angelcruz
  */
 public class Habitacion {
-    
+    private int numhabit;
+    private boolean estatus;
+    private String tipo;
+
+    public Habitacion(int numhabit, boolean estatus, String tipo) {
+        this.numhabit = numhabit;
+        this.estatus = estatus;
+        this.tipo = tipo;
+    }
+
+    public int getNumhabit() {
+        return numhabit;
+    }
+
+    public void setNumhabit(int numhabit) {
+        this.numhabit = numhabit;
+    }
+
+    public boolean isEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(boolean estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
