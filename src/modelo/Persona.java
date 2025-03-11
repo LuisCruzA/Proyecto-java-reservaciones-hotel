64d017d33e03906ef0a180f82a3113acfc1aac1a
CDCZ25 <146612810+CDCZ25@users.noreply.github.com>
2025-03-11 10:42:55 -0600

    Add files via upload

---
+++ src/modelo/Persona.java
@@ -0,0 +1,47 @@
+package pruebausuario;
+
+public abstract class Persona {
+    private String nombre;
+    private String genero;
+    private int telefono;
+    private String correo;
+
+    public Persona(String nombre, String genero, int telefono, String correo) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
