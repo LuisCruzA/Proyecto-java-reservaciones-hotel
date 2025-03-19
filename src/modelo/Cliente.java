dc13e3165e5b609bef9522c9928fc1df9451a09d
CDCZ25 <146612810+CDCZ25@users.noreply.github.com>
2025-03-18 19:31:22 -0600

    Update Cliente.java

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -10,10 +10,14 @@ package modelo;
  */
 public class Cliente extends Persona {
     private int id_usuario;
+    private int numTarjeta;
+    private int id_pago;
 
-    public Cliente(String nombre, String genero, int telefono, String correo, int id_usuario) {
+    public Cliente(String nombre, String genero, int telefono, String correo, int id_usuario, int numTarjeta, int id_pago) {
         super(nombre, genero, telefono, correo);
         this.id_usuario = id_usuario;
+        this.numTarjeta = numTarjeta;
+        this.id_pago = id_pago;
     }
 
     public int getId_usuario() {
@@ -24,6 +28,22 @@ public class Cliente extends Persona {
         this.id_usuario = id_usuario;
     }
 
+    public int getNumTarjeta() {
+        return numTarjeta;
+    }
+
+    public void setNumTarjeta(int numTarjeta) {
+        this.numTarjeta = numTarjeta;
+    }
+
+    public int getId_pago() {
+        return id_pago;
+    }
+
+    public void setId_pago(int id_pago) {
+        this.id_pago = id_pago;
+    }
+
     public void mostrar() {
         System.out.println("Detalles del Cliente");
         System.out.println("ID: " + getId_usuario());
@@ -31,5 +51,8 @@ public class Cliente extends Persona {
         System.out.println("Género: " + getGenero());
         System.out.println("Teléfono: " + getTelefono());
         System.out.println("Correo: " + getCorreo());
+        System.out.println("Tarjeta: " + getNumTarjeta());
+        System.out.println("ID Pago: " + getId_pago());
     }
 }
+
