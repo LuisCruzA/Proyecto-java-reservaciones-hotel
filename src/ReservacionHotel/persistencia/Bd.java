1e0b9777b70b0f3a68a565d6fb929c358e3a59a3
luis <langel18.1818@gmail.com>
2025-03-20 09:14:09 -0600

    se acomodaron paquetes y se implemento modelo capa

---
+++ src/ReservacionHotel/persistencia/Bd.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ReservacionHotel.persistencia;
+
+//aqui va la base de datos, puedeas hacerlo en otra calse si quieres. este es de prueba
+public class Bd {
+    
+}
